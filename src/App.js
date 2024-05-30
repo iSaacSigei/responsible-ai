5fbcaa29e8d384552cc3d171f18a74024ab423c8
iSaacSigei <kiplangatisack.is@gmail.com>
2024-05-31 04:14:30 +1000

    added navbae, footer and landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
 import './App.css';
+// index.js or App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Add other routes as needed */}
+      <Route path="/about" element={<div>About Page</div>} />
+      <Route path="/courses" element={<div>Courses Page</div>} />
+      <Route path="/resources" element={<div>Resources Page</div>} />
+      <Route path="/contact" element={<div>Contact Page</div>} />
+    </Routes>
+);
 
 export default App;
